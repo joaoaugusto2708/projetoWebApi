a1b6638058ebdd8700d31bf13b6b23c09e4a8777
João Augusto <joaorodriguesjf@hotmail.com>
2024-07-15 22:31:32 -0300

    Dominio Criado

---
+++ Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace projetoWebApi.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? Imagem { get; set; }
+}
+
---
+++ Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace projetoWebApi.Models;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public string? ImagemUrl { get; set; }
+    public decimal Preco { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+}
