0dddb95585910952b985f0ec3932e3ea421de683
João Augusto <joaorodriguesjf@hotmail.com>
2024-07-16 00:02:56 -0300

    Arrumando Serializações

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace projetoWebApi.Models;
 
@@ -23,5 +24,7 @@ public class Produto
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
     public int CategoriaId { get; set; }
+    //[JsonIgnore] Igonora na serialização e nas deserialização
+    [JsonIgnore]
     public Categoria? Categoria { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using projetoWebApi.Context;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => 
+        options.JsonSerializerOptions
+        //Ignora Referencia Ciclica
+            .ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
