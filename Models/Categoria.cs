ee53ee2669db30848923e4ad3b1e01674bccc9ef
João Augusto <joaorodriguesjf@hotmail.com>
2024-07-15 23:05:27 -0300

    Alterações de banco

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace projetoWebApi.Models;
 
+[Table("Categorias")]
 public class Categoria
 {
     public Categoria()
     {
         Produtos = new Collection<Produto>();
     }
+    [Key]
     public int CategoriaId { get; set; }
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? Imagem { get; set; }
     public ICollection<Produto>? Produtos { get; set; }
 }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,11 +1,24 @@
-﻿namespace projetoWebApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace projetoWebApi.Models;
+
+[Table("Produto")]
 public class Produto
 {
+    [Key]
     public int ProdutoId { get; set; }
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? Descricao { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
+    [Required]
+    [Column(TypeName="decimal(10,2)")]
     public decimal Preco { get; set; }
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
