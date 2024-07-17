a271ac46db982a60027863f0752184b62cdb576f
João Augusto <joaorodriguesjf@hotmail.com>
2024-07-16 22:12:44 -0300

    Utilizando métodos assincronos

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using projetoWebApi.Context;
 using projetoWebApi.Models;
 
@@ -15,51 +16,51 @@ namespace projetoWebApi.Controllers
             _appDbContext = context;
         }
         [HttpGet]
-        public ActionResult<IEnumerable<Produto>> Get()
+        public async Task<ActionResult<IEnumerable<Produto>>> Get()
         {
-            var produtos = _appDbContext.Produtos.Take(10).ToList();
+            var produtos =  await _appDbContext.Produtos.Take(10).ToListAsync();
             if (produtos is null)
                 return NotFound("Produtos não encontrados");
             return produtos;
         }
         [HttpGet("{id:int}", Name = "ObterProduto")]//Rota nomeado
-        public ActionResult<Produto> GetId(int id)
+        public async Task<ActionResult<Produto>> GetId(int id)
         {
-            var produto = _appDbContext.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            var produto = await _appDbContext.Produtos.FirstOrDefaultAsync(p => p.ProdutoId == id);
             if (produto == null)
                 return NotFound("Não foi encontrado o Produto");
             return produto;
         }
         [HttpPost]
-        public ActionResult Post([FromBody] Produto produto)
+        public async Task<ActionResult> Post([FromBody] Produto produto)
         {
             if (produto == null)
                 return BadRequest("Dados vazios");
 
-            _appDbContext.Produtos.Add(produto);
-            _appDbContext.SaveChanges();
+            await _appDbContext.Produtos.AddAsync(produto);
+            await _appDbContext.SaveChangesAsync();
             return new CreatedAtRouteResult("ObterProduto", //Utilização da Rota nomeado
                 new { id = produto.ProdutoId }, produto);
         }
         [HttpPut("{id:int}")]
-        public ActionResult Put(int id, Produto produto)
+        public async Task<ActionResult> Put(int id, Produto produto)
         {
             if(id != produto.ProdutoId)
                 return BadRequest("Id não confere");
             _appDbContext.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-            _appDbContext.SaveChanges();
+            await _appDbContext.SaveChangesAsync();
             return Ok(produto);
         }
 
         [HttpDelete("{id:int}")]
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
             var produto = _appDbContext.Produtos.FirstOrDefault(p => p.ProdutoId == id);
             if (produto is null)
                 return NotFound("Produto não encontrado");
 
             _appDbContext.Produtos.Remove(produto);
-            _appDbContext.SaveChanges();
+            await _appDbContext.SaveChangesAsync();
 
             return Ok(produto);
         }
